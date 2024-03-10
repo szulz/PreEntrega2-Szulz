089946ad6c2f757e619ef6438aaecd296d8daa7d
323Quiel! <ezeszulz@hotmail.com>
2024-03-10 14:51:40 -0300

    modified:   .gitignore 	modified:   src/components/NavBar/NavBarTop/NavBarTop.jsx

--- src/components/NavBar/NavBarTop/NavBarTop.jsx
+++ src/components/NavBar/NavBarTop/NavBarTop.jsx
@@ -5,13 +5,14 @@ import CartWidget from '../../CartWidget/CartWidget'
 const NavBarTop = () => {
     return (
         <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-            <a href="/" style={{ margin: "0px", color: 'inherit', textDecoration: 'none', cursor: 'pointer' }}><h1 >Koren Store</h1></a>
+            <Link to={'/'} style={{ margin: "0px", color: 'inherit', textDecoration: 'none', cursor: 'pointer' }}><h1 >Koren Store</h1></Link>
             <div>
                 <input type="text" placeholder="Search.."></input>
                 <button>Search</button>
             </div>
             <ul style={{ listStyleType: "none", padding: "0px", margin: "10px", display: "flex" }}>
-                <li style={{ marginRight: "10px" }}><a href="/">Home</a></li>
+                <li style={{ marginRight: "10px" }}><Link to={'/'}>Home</Link></li>
+                <Link to={'/'}></Link>
                 <li style={{ marginRight: "10px" }}><a href="#">About us</a></li>
                 <li style={{ marginRight: "10px" }}><a href="#">Contact</a></li>
             </ul>
